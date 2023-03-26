49eaf78fc9818ce20be418fb2c017d111c8f8d57
unknown <ashishsagwal19@gmail.com>
2023-03-26 13:23:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import Input_box from './components/Input_box';
+import BottomComponent from './components/BottomComponent';
+import ReactDOM from "react-dom";
+import React, {useState} from 'react'
 
 function App() {
+  const map = new Map();
+  const formDataChange = (data) => {
+    if(map.has(data.uId)) {
+      map.get(data.uId).name = data.name;
+      map.get(data.uId).value = data.value;
+    } else {
+      map.set(data.uId, data);
+    }
+  };
+
+  const [res, setRes] = useState(undefined);
+  const finalRes = (data) => {
+    setRes(data);
+  };
+
+  let id = 1;
+  map.set(id, {name: "My Args", value: "false", uId:id});
+  const handleButtonClick = () => {
+    const rootElement = document.getElementById(id.toString());
+    id++;
+    const newId = id.toString();
+    ReactDOM.render(<><Input_box onDataChange={formDataChange} uId={id}/><div id={newId}></div></>, rootElement);
+    map.set(id, {name: "My Args", value: "false", uId:id});
+  };
+
+  const handleCancelButtonClick = () => {
+    const rootElement = document.getElementById("dropDown");
+    ReactDOM.render(<BottomComponent map={map} finalRes={finalRes} id="dropDown"/>, rootElement);
+    finalRes(undefined);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+      <Input_box onDataChange={formDataChange} uId={id}/>
+      </div>
+      <div id="1"></div>
+      <div>
+      <button type="button" onClick={handleButtonClick} className="btn btn-outline-secondary">+ add args</button>
+      </div>
+      <br/>
+      <BottomComponent map={map} finalRes={finalRes} id="dropDown"/>  
+      <button type="button" onClick={handleCancelButtonClick} className="btn btn-outline-secondary"> X </button>
+      <br/>
+      <div>result : {res === undefined ? "undefined" : (res ? "true" : "false")}</div>
     </div>
   );
 }
